e137cfc9badbd21ac96e80158d19553d6d0322e8
Isaac Verminck <isaacverm@gmail.com>
2018-04-06 12:17:17 +0000

    Add time difference age marriage

--- R/visualize.R
+++ R/visualize.R
@@ -1,12 +1,12 @@
 only_main_regions <- function(df) {
-  df %>% 
+  df %>%
     filter(region %in% c("flanders","wallonia","brussels"))
 }
 
 age_continuous <- function(df) {
-  df %>% 
-    filter(!(age %in% c("60 en +","12-"))) %>% 
-    mutate(age = as.numeric(as.character(age))) %>% 
+  df %>%
+    filter(!(age %in% c("60 en +","12-"))) %>%
+    mutate(age = as.numeric(as.character(age))) %>%
     filter(age >= 18)
 }
 
@@ -18,6 +18,12 @@ percentage_of_total_by_region <- function(df_without_perc) {
   df_with_perc <- df_without_perc %>%
     left_join(., nr_marriages_by_region) %>%
     mutate(percentage = round((quantity/total_marriages)*100,
-                              digits = 2)) %>% 
+                              digits = 2)) %>%
     select(-one_of(c("total_marriages")))
+}
+
+year_to_year_percentage_differences <- function(df) {
+  df %>%
+    group_by(age, region) %>%
+    summarise(difference = diff(percentage))
 }
